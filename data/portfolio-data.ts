d88664b69eaa62a1507d816144b69e97cbb055c7
Ələmdar Manafov <alamdarmanafov@gmail.com>
2025-05-27 17:01:29 +0400

    Message

--- data/portfolio-data.ts
+++ data/portfolio-data.ts
@@ -14,7 +14,7 @@ export const portfolioData = {
       id: "1",
       title: "BePositive.az",
       description: "Social media marketing and branding company",
-      image: "/img/portfolio/bepositive.jpg",
+      image: "/img/portfolio/1.svg",
       category: "Branding",
       link: "https://bepositive.az",
       technologies: ["Social Media", "Branding", "Marketing"]
@@ -23,7 +23,7 @@ export const portfolioData = {
       id: "2",
       title: "Why #BePositive",
       description: "Book project and marketing strategy",
-      image: "/img/portfolio/book.jpg",
+      image: "/img/portfolio/2.svg",
       category: "Publishing",
       link: "https://novella.az/kitab/niye-bepositive",
       technologies: ["Book", "Marketing", "Strategy"]
@@ -32,7 +32,7 @@ export const portfolioData = {
       id: "3",
       title: "Social Media Trainings",
       description: "Social media trainings and seminars",
-      image: "/img/portfolio/training.jpg",
+      image: "/img/portfolio/3.svg",
       category: "Education",
       link: "https://wa.me/994105310129",
       technologies: ["Training", "Social Media", "Education"]
@@ -43,7 +43,7 @@ export const portfolioData = {
       id: "1",
       title: "BePositive.az",
       description: "Reklam Agentliyi",
-      image: "/img/portfolio/bepositive.jpg",
+      image: "/img/portfolio/1.svg",
       category: "Agentlik",
       link: "https://bepositive.az",
       technologies: ["Sosial Media", "Brendinq", "Marketinq"]
@@ -52,7 +52,7 @@ export const portfolioData = {
       id: "2",
       title: "Niyə görə #BePositive",
       description: "Kitab layihəsi və marketinq strategiyası",
-      image: "/img/portfolio/book.jpg",
+      image: "/img/portfolio/2.svg",
       category: "Nəşriyyat",
       link: "https://novella.az/kitab/niye-bepositive",
       technologies: ["Kitab", "Marketinq", "Strategiya"]
@@ -61,7 +61,7 @@ export const portfolioData = {
       id: "3",
       title: "Sosial Media Təlimləri",
       description: "Sosial media üzrə təlimlər və seminarlar",
-      image: "/img/portfolio/training.jpg",
+      image: "/img/portfolio/3.svg",
       category: "Təhsil",
       link: "https://wa.me/994105310129",
       technologies: ["Təlim", "Sosial Media", "Təhsil"]
