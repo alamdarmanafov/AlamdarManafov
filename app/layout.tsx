ea219d73a8fcc1d19d485b3bc789cbaf284e122b
Ələmdar Manafov <alamdarmanafov@gmail.com>
2025-09-15 06:37:14 +0400

    Sayt üzərində düzəlişlər

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,25 @@ export const metadata: Metadata = {
   title: "Ələmdar Manafov | Rəqəmsal Marketinq Eksperti",
   description:
     "Portfolio of Alamdar Manafov - 8+ years Marketing Strategist | Branding & Digital Expert | Founder of Be Positive Company.",
-  keywords: ["Alamdar Manafov", "Ələmdar Manafov" , "Digital Marketing Expert", "Marketing Strategist" , "Branding & Digital Expert" , "Social media advisor", "Portfolio", "Digital Experiences","sosial media məsləhətçisi", "smm", "Blogger", "researcher"],
+keywords: ["Alamdar Manafov", "Ələmdar Manafov" , "Digital Marketing Expert", "Marketing Strategist" , 
+    "Branding & Digital Expert" , "Social media advisor", "Portfolio", "Digital Experiences",
+    "sosial media məsləhətçisi", "smm", "Blogger", "researcher", "sloqan bepositive", "blogger", "marketinq expert", 
+    "marketinq konsultant", "be positive advertising agency", "bepositive.az", "rəqəmsal marketinq agentliyi"
+  , "sosial media idarəetməsi", "brendinq və dizayn", "smm mütəxəssis", "reels çəkilişi və montaj", "kontent yaradılması"
+, "reklam planlaması", "influencer marketing", "biznes üçün sosial media", "kiçik biznes üçün marketing", "brend tanıtımı xidmətləri"
+, "satış artıran reklam", "reklam xidməti", "marketing agency", "digital marketing", "brendinq agentliyi"
+, "sosial media reklamı", "kInstagram reels çəkilişi", "sosial media strategiyası", "loqo dizayn"
+, "logo dizayn", "instagram reklam", "tiktok reklam"
+, "facebook reklam", "meta reklam", "google reklam", "seo analiz" , "seo anlysis" , "website develop" 
+, "marketing specialist" , "social media strategist" , "digital marketing expert" , "brand strategistm" , "content creator" 
+, "rəqəmsal marketinq mütəxəssisi" , "sosial media təlimçi" , "smm mentor" , "smm təlim" , "rəqəmsal marketinq təlimləri" , "sosial media kursları" 
+, "Be Positive founder" , "kreativ sahibkar" , "be pozitiv brend hekayəsi" , "motivational speaker" , "personal brand strategist " 
+, "rəqəmsal brend qurucusu" , "content marketing expert" , "məzmun yaradıcısı" , "digital marketing consultant" 
+, "social media growth expert" , "creative marketing solutions" , "biznes üçün kontent istehsalı" , "social media ads specialist" 
+, "SMM masterclass", "marketing mentor", "digital marketing trainer", "marketing manager"
+, "rəqəmsal reklam təlimi" , "Azerbaijani marketing expert", "international marketing consultant"],
+
+
   openGraph: {
     title: "Ələmdar Manafov | Rəqəmsal Marketinq Eksperti",
     description:
